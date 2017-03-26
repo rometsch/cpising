bab21fc057d753fa710c5cf9333a254dd39493d6
Thomas Rometsch <thomas.rometsch@gmail.com>
2017-03-26 14:00:48 +0200

    analysis of thermalisation for multihit

---
+++ ising/multihit/thermalization/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Run calculations
+#../../Release/cplab-ising-2dising test1_config.txt > result1.txt
+../../Release/cplab-ising-2dising test2_config.txt > result2.txt
+
+# Plot
+#gnuplot plot.plt
